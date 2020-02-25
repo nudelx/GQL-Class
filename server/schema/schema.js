35c466583881639b02d5edbe81c54a88d87527db
nudelx <nudelx@gmail.com>
2020-02-25 17:12:02 +0200

    step2

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -8,19 +8,7 @@ const {
   GraphQLList
 } = graphql
 
-const db = {
-  books: [
-    { name: 'AAAA', genre: 'aaaa', id: '1', author_id: '1' },
-    { name: 'BBBB', genre: 'bbbb', id: '2', author_id: '2' },
-    { name: 'CCCC', genre: 'cccc', id: '3', author_id: '3' },
-    { name: 'DDDD', genre: 'dddd', id: '4', author_id: '1' }
-  ],
-  authors: [
-    { name: 'Author AAAA', age: 22, id: '1', book_id: '1' },
-    { name: 'Author BBBB', age: 33, id: '2', book_id: '2' },
-    { name: 'Author CCCC', age: 44, id: '3', book_id: '3' }
-  ]
-}
+const db = require('../db/db.js')
 
 const BookType = new GraphQLObjectType({
   name: 'Book',
@@ -67,6 +55,18 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLID } },
       resolve: (parent, args) => {
         return db.authors.find(item => item.id === args.id)
+      }
+    },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve: (parent, args) => {
+        return db.books
+      }
+    },
+    authors: {
+      type: new GraphQLList(AuthorType),
+      resolve: (parent, args) => {
+        return db.authors
       }
     }
   }
