84d943969415dbb4ed86c2976c533c5137d32371
nudelx <nudelx@gmail.com>
2020-02-26 18:50:48 +0200

    clean connection

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -3,7 +3,6 @@ const { GraphQLSchema } = graphql
 const RootQuery = require('./rootQuery')
 const Mutation = require('./mutation')
 const connectDb = require('../db/mongo/mongoDb')()
-
 // const db = require('../db/db.js')
 
 module.exports = new GraphQLSchema({ query: RootQuery, mutation: Mutation })
